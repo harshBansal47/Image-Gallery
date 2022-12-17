0a9c4bc3354c9947f04563c1c14e682567f554f2
Harsh Bansal <harshbansal472000@gmail.com>
2022-12-17 06:03:09 +0530

    loader inside is added

--- script1.js
+++ script1.js
@@ -9,11 +9,13 @@ let next_url = '';
 let search_value = '';
 
 // Default Content on web page
-// on loading 
 let loadContent = ()=>{
-    document.addEventListener('DOMContentLoaded',async()=>{
+    document.addEventListener('DOMContentLoaded',async()=>{     
         let data = await fetchData(baseUrl);
         generateHTML(data.photos);
+        for(let i=0; i<loader2.length;i++){
+            loader2[i].style.display = 'none';
+        }
     });
 }
 loadContent();
@@ -28,30 +30,32 @@ let fetchData = async(url)=>{
                       Authorization:api_key }}
                       );
     let data = await res.json();
+    //getting prev and next url from json object
     prev_url = data.prev_page;
     next_url = data.next_page;
     return data;         
 }
-
+let loader2;
 // Function For generating html
 let gallery = document.querySelector('.gallery');
 let generateHTML = (data)=>{
     gallery.innerHTML="";
     //here data is a array of photos object
-    console.log(data);
     data.forEach(element => {
         let div = document.createElement('div');
+        //loader
         div.classList.add('item');
         div.innerHTML = `
+        <div class="loader2"><iframe src="https://embed.lottiefiles.com/animation/99571"></iframe></div>
         <a href = "${element.url}"><img src = "${element.src.original}"></a>
         <div class = "imgbottom">
             <h4><a href="${element.photographer_url}">${element.photographer}</a></h4>
             <h4><a  download="img1"><span class="material-symbols-outlined">download</span></a></h4>
         </div>`;
-        gallery.appendChild(div);
+        gallery.appendChild(div);     
     });
+    loader2 = document.getElementsByClassName('loader2'); 
 }
-
 //Function for search query
 const input_Element = document.getElementById('inputSearch');
 // Search Function
@@ -87,4 +91,9 @@ handleNext=async()=>{
     baseUrl = next_url;
     let jsonData = await fetchData(baseUrl);
     generateHTML(jsonData.photos);
+}
+
+//hideloader function
+const hideloader = ()=>{
+    document.querySelectorAll('.loader2').style.display = "none";
 }
